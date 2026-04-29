865743da3b4152e9bb0abc9cb94d1dc1cec7ebf9
Alocad0 <34124866+Alocad0@users.noreply.github.com>
2026-04-29 14:40:41 +0200

    Add MPlusModel to __all__ exports

--- source/kalden/core/mike/__init__.py
+++ source/kalden/core/mike/__init__.py
@@ -1,4 +1,10 @@
 from . import timeseries
 from .timeseries import EUM, Dfs0
+from .mike_plus import MPlusModel
 
-__all__ = ["timeseries", "EUM", "Dfs0"]
+__all__ = [
+    "timeseries",
+    "Dfs0",
+    "EUM",
+    "MPlusModel",
+]
