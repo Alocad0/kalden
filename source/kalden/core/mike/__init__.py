b5651fb453fd3833ba3e5ffbda91b162a5dfdd67
Alocad0 <34124866+Alocad0@users.noreply.github.com>
2026-04-29 14:41:29 +0200

    Comment out MPlusModel import in __init__.py
    Comment out the import of MPlusModel from mike_plus.

--- source/kalden/core/mike/__init__.py
+++ source/kalden/core/mike/__init__.py
@@ -1,10 +1,10 @@
 from . import timeseries
 from .timeseries import EUM, Dfs0
-from .mike_plus import MPlusModel
+#from .mike_plus import MPlusModel
 
 __all__ = [
     "timeseries",
     "Dfs0",
     "EUM",
-    "MPlusModel",
+#    "MPlusModel",
 ]
