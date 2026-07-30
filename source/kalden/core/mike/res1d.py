ae1013124803622997f328736fdb75376b450db6
Alocad0 <34124866+Alocad0@users.noreply.github.com>
2026-07-30 13:08:00 +0000

    rehabilitate loading warning message after return fix

--- source/kalden/core/mike/res1d.py
+++ source/kalden/core/mike/res1d.py
@@ -1029,9 +1029,9 @@ class Res1D:
         if self.load_mode == "full":
             return "full"
         else:
-            # Prints warning message -> also abandoned because annoying
-            # print("Filtered / auto load modes are deactivated because there is no real lazy-loading available in mikeio1d.")
-            # print(f"File is fully loaded in memory ({self.file_size_bytes/1e6:.0f} MB)")
+            # Prints warning message
+            print("Filtered loadind mode is deactivated because there is no real lazy-loading available in mikeio1d.")
+            print(f"File is in effect fully loaded into memory ({self.file_size_bytes/1e6:.0f} MB)")
             return "full"
 
             # if self.load_mode == "auto":
